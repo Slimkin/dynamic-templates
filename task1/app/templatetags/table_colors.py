9be8a4ce36faa4d7ddcc4ed669929aa57404f08a
Slimkin <doc.slimkin.dgi@gmail.com>
2020-08-03 18:28:38 +0300

    add all

---
+++ task1/app/templatetags/table_colors.py
@@ -0,0 +1,20 @@
+from django.template import Library
+
+register = Library()
+
+@register.filter(name='tc')
+def set_color(value):
+    try:
+        value = float(value)
+    except ValueError:
+        return ''
+    if value < 0:
+        return '#0f7003'
+    elif 1 <= value < 2:
+        return '#fbc8c9'
+    elif 2 <= value < 5:
+        return '#f6767a'
+    elif value >= 5:
+        return '#fb0008'
+    else:
+        return ''
---
+++ task1/app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+import csv
+
+def inflation_view(request):
+    template_name = 'inflation.html'
+
+    with open('./inflation_russia.csv', encoding='utf-8') as f:
+        inflation = list(csv.reader(f, delimiter=';'))
+
+    context = {
+        'table_header': inflation[0],
+        'table_body': inflation[1:],
+    }
+
+    return render(request, template_name,
+                  context)
